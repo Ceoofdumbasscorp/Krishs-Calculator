7eeb3220347d4602c3d8492fe562d08842a19c97
Ceoofdumbasscorp <156059249+Ceoofdumbasscorp@users.noreply.github.com>
2025-12-30 13:17:13 -0800

    Add files via upload

--- calc.py
+++ calc.py
@@ -5,23 +5,30 @@ j = 0
 operations = ["+", "-", "*", "/"]
 for op in operations:
     print(op)
-number1 = int(input("Please enter the first number you want: "))
-op = input("What Operator would you like? ")
-number2 = int(input("Please enter the second number you want: "))
+validRun = True
+while(validRun):
+    number1 = int(input("Please enter the first number you want: "))
+    op = input("What Operator would you like? ")
+    number2 = int(input("Please enter the second number you want: "))
 
-if op == "+":
-    sum = number1 + number2
-elif op == "-":
-    sum = number1 - number2
-elif op == "*":
-    sum = number1 * number2
-elif op == "/":
-    sum = number1 / number2
-else:
-    print("INVALID OPERATIONS TRY AGAIN")
+    if op == "+":
+        sum = number1 + number2
+    elif op == "-":
+        sum = number1 - number2
+    elif op == "*":
+        sum = number1 * number2
+    elif op == "/":
+        sum = number1 / number2
+        print("INVALID OPERATIONS TRY AGAIN")
 
-calc = "Calculating"
-for i in range(3):
-    calc += "."
-print(calc)
-print(f"The answer to {number1} {op} {number2} is: {sum}")
+    calc = "Calculating"
+    for i in range(3):
+        calc += "."
+    print(calc)
+    print(f"The answer to {number1} {op} {number2} is: {sum}")
+
+    runAgain = input("Would you like to perform another calculation? (y/n): ")
+    if runAgain.lower() != 'y':
+        validRun = False
+        print("Thank you for using The Krish Calculator! Goodbye!")
+    
