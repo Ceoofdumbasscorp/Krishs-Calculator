4f4ec60487a3bd9e639c37049637af9725aad6f9
Ceoofdumbasscorp <156059249+Ceoofdumbasscorp@users.noreply.github.com>
2025-12-29 19:01:47 -0800

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,27 @@
+print("Welcome to The Krish Calculator!" +
+      "\nThese are the allowed operations: ")
+i = 0
+j = 0
+operations = ["+", "-", "*", "/"]
+for op in operations:
+    print(op)
+number1 = int(input("Please enter the first number you want: "))
+op = input("What Operator would you like? ")
+number2 = int(input("Please enter the second number you want: "))
+
+if op == "+":
+    sum = number1 + number2
+elif op == "-":
+    sum = number1 - number2
+elif op == "*":
+    sum = number1 * number2
+elif op == "/":
+    sum = number1 / number2
+else:
+    print("INVALID OPERATIONS TRY AGAIN")
+
+calc = "Calculating"
+for i in range(3):
+    calc += "."
+print(calc)
+print(f"The answer to {number1} {op} {number2} is: {sum}")
